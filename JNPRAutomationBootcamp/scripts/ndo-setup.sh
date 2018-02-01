446c601c3b1ba84659fc785a60ce601e204944da
Hector Isaza <Hector@MAC-2.local>
2018-02-01 12:05:55 -0500

    bug fix
    Not sure why it fixed this issue but it did
    Fixed this issue:
    
     ndo:   Could not find .egg-info directory in install record for
    ncclient>=0.5.3 (from junos-eznc)
        ndo: Compiling /tmp/pip_build_root/jinja2/jinja2/asyncfilters.py ...
        ndo:   File "/tmp/pip_build_root/jinja2/jinja2/asyncfilters.py",
    line 7
        ndo:     async def auto_to_seq(value):
        ndo:             ^
        ndo: SyntaxError: invalid syntax

--- JNPRAutomationBootcamp/scripts/ndo-setup.sh
+++ JNPRAutomationBootcamp/scripts/ndo-setup.sh
@@ -29,6 +29,7 @@ sudo ifup eth1
 sudo apt-get update
 sudo apt-get install -y python-dev libxml2-dev python-pip libxslt-dev build-essential libssl-dev libffi-dev libffi-dev
 sudo pip install --upgrade setuptools
+sudo pip install --upgrade setuptools pip
 sudo pip install markupsafe
 sudo pip install cryptography==1.2.1 junos-eznc ansible==2.3.2.0 jxmlease
 sudo ansible-galaxy --force install Juniper.junos,1.4.3
