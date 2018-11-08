b56f9a58c3b5ad765171828ccbd8b37712729364
darien-hirotsu <darien@sdnessentials.com>
2018-11-08 15:06:27 -0500

    Fixing conflicts and adding SRX01 patch

--- JNPRAutomationBootcamp/scripts/patch_automation_bootcamp.sh
+++ JNPRAutomationBootcamp/scripts/patch_automation_bootcamp.sh
@@ -3,9 +3,6 @@
 sudo pip install virtualenv
 mkdir ~/.virtualenvs
 sudo pip install virtualenvwrapper
-<<<<<<< HEAD
-=======
 export WORKON_HOME=~/.virtualenvs
 . /usr/local/bin/virtualenvwrapper.sh
->>>>>>> 9d5b195a181f5da6323880bfae023539c72256e0
 mkvirtualenv automation_bootcamp
